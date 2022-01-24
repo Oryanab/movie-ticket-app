b9c3a881d38b370178782c905f257357be9baa57
Oryanab <86202122+Oryanab@users.noreply.github.com>
2022-01-24 11:45:06 +0200

    completed template 2.0

--- frontend/src/Redux/MiddleWares/counterMiddleware.ts
+++ frontend/src/Redux/MiddleWares/counterMiddleware.ts
@@ -2,7 +2,7 @@ import { Middleware } from 'redux';
 
 export const isNumber: Middleware = store => next => action => {
   if (action.shouldConfirm) {
-    if (confirm('are you sure?')) {
+    if (confirm(`are you sure you want to ${action.type} by ${action.payload}?`)) {
       next(action);
     }
   } else {
