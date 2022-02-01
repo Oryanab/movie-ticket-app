602c8c819350d4ef2d453fb85f49a86b5c7020b3
Oryanab <86202122+Oryanab@users.noreply.github.com>
2022-02-01 13:09:16 +0200

    updated frontend 07

--- frontend/src/Components/child-components/Movies.tsx
+++ frontend/src/Components/child-components/Movies.tsx
@@ -1,21 +1,31 @@
+import { AnyRecord } from 'dns';
 import React, { useState } from 'react';
 import { Card, ListGroupItem, ListGroup, Form, Button } from 'react-bootstrap';
 import { Movies } from '../../Redux/Types/generalTypes';
 import { movies, createUniqueMovieItemArray, createUniqueMovieDateArray } from '../../Utils/movieUtils';
+
 interface TimeObject {
   time_start: string;
   movieId: string;
 }
 
+interface DateItem {
+  key: string;
+  value: Array<TimeObject>;
+}
+
 export default function Movie() {
-  const [selectedDate, setSelectedDate] = useState({});
-  const [availableHours, setAvailableHours] = useState({});
+  const [selectedDate, setSelectedDate] = useState<string>(new Date().toLocaleDateString());
+  const initialAvailableHours = [{ time_start: '', movieId: 'undefined' }];
+  const [availableHours, setAvailableHours] = useState<Array<TimeObject>>(initialAvailableHours);
+  const [movieId, setMovieId] = useState<string>('');
+
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
       <>
         {createUniqueMovieItemArray(movies).map(movie => (
           <>
-            <Card key={movie.data!.movieId} style={{ width: '18rem', margin: '3vw' }}>
+            <Card id={movie.data!.movie_title} key={movie.data!.movie_title} style={{ width: '18rem', margin: '3vw' }}>
               <Card.Img variant="top" height={'250px'} src={movie.data!.img} />
               <Card.Body>
                 <Card.Title>{movie.data!.movie_title}</Card.Title>
@@ -36,25 +46,70 @@ export default function Movie() {
               </ListGroup>
               <Card.Body>
                 <p style={{ marginTop: '-1vh' }}>Choose date/time:</p>
-                <Form.Control
+                {/* <Form.Control
+                  id={movie.data!.movie_title}
                   style={{ marginTop: '-1vh' }}
                   onChange={e => {
-                    setSelectedDate(new Date(e.target.value).toLocaleDateString());
+                    const chosenDate = e.target.value;
+                    setSelectedDate(new Date(chosenDate).toLocaleDateString());
+                    if (movie.data!.movie_date.toLocaleDateString() === selectedDate) {
+                      setAvailableHours(
+                        createUniqueMovieDateArray(movie).find(
+                          item => new Date(item.key).toLocaleDateString() === selectedDate
+                        )?.value
+                      );
+                    }
                   }}
                   size="sm"
                   type="date"
                   placeholder="Small text"
-                />
-                <Form.Select style={{ marginTop: '1vh' }} size="sm">
+                /> */}
+                <Form.Select
+                  onChange={(e: any) => {
+                    const chosenDate = e.target.value;
+                    setSelectedDate(new Date(chosenDate).toLocaleDateString());
+                    console.log(selectedDate);
+                    if (chosenDate === 'Please Select Date') {
+                      setAvailableHours(initialAvailableHours);
+                    } else {
+                      setAvailableHours(
+                        createUniqueMovieDateArray(movie).find(
+                          item => new Date(item.key).toLocaleDateString() === chosenDate
+                        )?.value
+                      );
+                    }
+                  }}
+                  style={{ marginTop: '1vh' }}
+                  size="sm"
+                >
                   <>
-                    {createUniqueMovieDateArray(movie).forEach(item => {
-                      if (new Date(item.key).toLocaleDateString() === selectedDate) {
-                        item.value.map((time: TimeObject) => <option key={time.movieId}>{time.time_start}</option>);
-                      }
-                    })}
+                    <option key="default">Please Select Date</option>
+                    {createUniqueMovieDateArray(movie).map((dateItem: DateItem) => (
+                      <option key={dateItem.key}>{new Date(dateItem.key).toLocaleDateString()}</option>
+                    ))}
                   </>
                 </Form.Select>
-                <Button style={{ marginTop: '1vh', fontSize: '1.6vh' }} variant="primary">
+                <Form.Select
+                  onChange={e => {
+                    setMovieId(e.currentTarget.options[e.currentTarget.options.selectedIndex].id);
+                  }}
+                  style={{ marginTop: '1vh' }}
+                  size="sm"
+                >
+                  <>
+                    <option key="default">Please Select Time</option>
+                    {availableHours.map((time: TimeObject) => (
+                      <option id={time.movieId} key={time.movieId}>
+                        {time.time_start}
+                      </option>
+                    ))}
+                  </>
+                </Form.Select>
+                <Button
+                  href={movieId.length > 0 ? `http://localhost:3000/tickets/${movieId}` : `http://localhost:3000/`}
+                  style={{ marginTop: '1vh', fontSize: '1.6vh' }}
+                  variant="primary"
+                >
                   Get Tickets
                 </Button>
               </Card.Body>
