83057500f60951f1d430b7aae7d8e657dcff6c83
Oryanab <86202122+Oryanab@users.noreply.github.com>
2022-02-02 01:19:22 +0200

    navbar frontend 01

--- frontend/src/Components/child-components/Navbar.tsx
+++ frontend/src/Components/child-components/Navbar.tsx
@@ -75,10 +75,37 @@ export default function Navbar() {
     //     </Row>
     //   </Container>
     // </Nav>
-    <nav className="navbar navbar-dark bg-dark pb-0">
-      <i className="navbar-brand mx-auto text-center">
-        <div>Movie Ticket App</div>
-      </i>
-    </nav>
+    // <nav style={{ display: 'flex' }} className="navbar navbar-dark bg-dark pb-0">
+    //   <i style={{ display: 'flex' }} className="navbar-brand mx-auto text-center">
+    //     <div>Movie Ticket App</div>
+    //     <div>Movie Ticket App</div>
+    //     <div>Movie Ticket App</div>
+    //   </i>
+    //   <i className="navbar-brand mx-auto text-center">
+    //     <div>Movie Ticket App</div>
+    //   </i>
+    // </nav>
+    <Nav activeKey="/home" className="navbar navbar-dark bg-dark pb-0">
+      <Nav.Item style={{ display: 'flex', marginLeft: '1vw' }}>
+        <Nav.Link style={{ backgroundColor: 'white', margin: '0.2vw' }} href="/">
+          Homepage
+        </Nav.Link>
+        <Nav.Link style={{ backgroundColor: 'white', margin: '0.2vw' }} href="/ticket-panel">
+          My Tickets
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item style={{ color: 'white', marginLeft: '-6vw' }}>
+        <h5>Movie Ticket App</h5>
+      </Nav.Item>
+      <Nav.Item style={{ display: 'flex', marginRight: '4vw' }}>
+        <Nav.Link
+          style={{ backgroundColor: 'white', margin: '0.2vw' }}
+          target="blank"
+          href="https://github.com/Oryanab/movie-ticket-app"
+        >
+          Github
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
